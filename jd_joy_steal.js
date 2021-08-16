b5bc09a8b881a7d44146bade480714d84f353e63
feverrun <209981505@qq.com>
2021-08-16 17:46:16 +0800

    update

--- jd_joy_steal.js
+++ jd_joy_steal.js
@@ -1,3 +1,4 @@
+// @grant    require
 /*
 Last Modified time: 2021-6-6 10:22:37
 活动入口：京东APP我的-更多工具-宠汪汪
@@ -27,7 +28,7 @@ const $ = new Env('宠汪汪偷好友积分与狗粮');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-const validator = require('./JDJRValidator_Pure.js');
+const validator = require('./JDJRValidator_Smiek.js');
 let nowTimes = new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000);
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
@@ -286,7 +287,7 @@ async function helpFriendsFeed() {
 }
 function getFriends(currentPage = '1') {
   return new Promise(resolve => {
-    const url = `${JD_API_HOST}/common/pet/h5/getFriends?itemsPerPage=20&currentPage=${currentPage * 1}&reqSource=h5&invokeKey=qRKHmL4sna8ZOP9F`
+    const url = `${JD_API_HOST}/common/pet/h5/getFriends?itemsPerPage=20&currentPage=${currentPage * 1}&reqSource=h5&invokeKey=ztmFUCxcPMNyUq0P`
     const options = {
       url,
       headers: {
@@ -465,7 +466,7 @@ function getRandomFood(friendPin) {
 }
 function getCoinChanges() {
   return new Promise(resolve => {
-    const url = `${JD_API_HOST}/common/pet/getCoinChanges?changeDate=${Date.now()}&reqSource=h5&invokeKey=qRKHmL4sna8ZOP9F`
+    const url = `${JD_API_HOST}/common/pet/getCoinChanges?changeDate=${Date.now()}&reqSource=h5&invokeKey=ztmFUCxcPMNyUq0P`
     const options = {
       url,
       headers: {
@@ -583,7 +584,7 @@ function TotalBean() {
   })
 }
 function taskUrl(functionId, friendPin) {
-  const url = `${JD_API_HOST}/common/pet/${functionId}?reqSource=h5&invokeKey=qRKHmL4sna8ZOP9F${friendPin && "&friendPin=" + encodeURI(friendPin)}`
+  const url = `${JD_API_HOST}/common/pet/${functionId}?reqSource=h5&invokeKey=ztmFUCxcPMNyUq0P${friendPin && "&friendPin=" + encodeURI(friendPin)}`
   return {
     url,
     headers: {
