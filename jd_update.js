4a34d87036f3fcba9d6ada23aa20c274c546960f
feverrun <209981505@qq.com>
2021-08-17 23:07:41 +0800

    update

--- jd_update.js
+++ jd_update.js
@@ -6,7 +6,7 @@
 const exec = require('child_process').exec;
 
 if(process.env.PWD==='/ql/scripts'){
-  exec("cd /ql/repo/feverrun_jd_scripts")
+  exec("cd /ql/repo/feverrun_my_script")
 }
 
 exec("git fetch --all; git reset --hard origin/main; git pull", (error, stdout, stderr) => {
@@ -15,7 +15,7 @@ exec("git fetch --all; git reset --hard origin/main; git pull", (error, stdout, 
 
 if(process.env.PWD==='/ql/scripts') {
   if (__dirname.indexOf('/ql/') > -1) {
-    exec('ql repo https://github.com/feverrun/my_scripts.git "jd_|jx_|getCookie" "activity|backUp|Coupon|enen" "^jd[^_]|USER|tools"', (error, stdout, stderr) => {
+    exec('ql repo https://github.com/feverrun/my_script.git "jd_|jx_|getCookie" "activity|backUp|Coupon|enen" "^jd[^_]|USER|tools"', (error, stdout, stderr) => {
       console.log(stdout.trim())
     })
   }
