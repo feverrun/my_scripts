1b9fd5d1f9916374451cd2f90151ec843c6a8c51
feverrun <209981505@qq.com>
2021-08-21 07:37:25 +0800

    更新

--- jd_joy_steal.js
+++ jd_joy_steal.js
@@ -27,7 +27,7 @@ const $ = new Env('宠汪汪偷好友积分与狗粮');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-const validator = require('./JDJRValidator_Pure.js');
+const validator = require('./utils/JDJRValidator_Pure');
 let nowTimes = new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000);
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
