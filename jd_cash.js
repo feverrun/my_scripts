6cc96dc654a4d9c673e412bc90421801fb1e21f4
feverrun <209981505@qq.com>
2021-09-04 17:21:48 +0800

    update

--- jd_cash.js
+++ jd_cash.js
@@ -17,7 +17,7 @@ let cookiesArr = [], cookie = '', message;
 let helpAuthor = false;
 const randomCount = $.isNode() ? 5 : 5;
 let cash_exchange = false;//是否消耗2元红包兑换200京豆，默认否
-const inviteCodes = [""]
+const inviteCodes = ["IhM_au67bvQm82-6iw"]
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
     cookiesArr.push(jdCookieNode[item])
