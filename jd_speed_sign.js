feb4db67ffa8199cb70687d9b8a56aa9edf98d93
feverrun <209981505@qq.com>
2021-09-06 21:02:56 +0800

    update

--- jd_speed_sign.js
+++ jd_speed_sign.js
@@ -34,6 +34,8 @@ if ($.isNode()) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
   }
+  console.log(cookiesArr);
+  return;
   for (let i = 1; i <= cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
