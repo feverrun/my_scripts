367bfec8deb95feb13aea23fdd642134cc4ffedb
feverrun <209981505@qq.com>
2021-09-07 19:35:27 +0800

    update

--- gua_opencard24.js
+++ gua_opencard24.js
@@ -35,8 +35,8 @@ guaopencard24="true"
 
 */
 const $ = new Env('会员“食”力派');
-const Faker=require('./sign_graphics_validate.js') 
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
+const Faker=require('./utils/sign_graphics_validate.js')
 
 const notify = $.isNode() ? require('./sendNotify') : '';
 //IOS等用户直接用NobyDa的jd cookie
