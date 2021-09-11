187bf5ea401023edbe6e69fbc21d7baa6dc1d07a
feverrun <209981505@qq.com>
2021-09-11 15:45:39 +0800

    update

--- jd_redPacket.js
+++ jd_redPacket.js
@@ -1,31 +1,18 @@
 /*
 京东全民开红包。
-Last Modified time: 2021-05-19 16:27:18
 活动入口：京东APP首页-领券-锦鲤红包。
 内部互助后若有剩余助力作者
 未实现功能：领3张券功能
 
-脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
-================QuantumultX==================
-[task_local]
-#京东全民开红包
-1 0,3 * * * jd_redPacket.js, tag=京东全民开红包, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jd_redPacket.png, enabled=true
-===================Loon==============
-[Script]
 cron "1 0,3 * * *" script-path=jd_redPacket.js, tag=京东全民开红包
-===============Surge===============
-[Script]
-京东全民开红包 = type=cron,cronexp="1 0,3 * * *",wake-system=1,timeout=3600,script-path=jd_redPacket.js
-====================================小火箭=============================
-京东全民开红包 = type=cron,script-path=jd_redPacket.js, cronexpr="1 0,3 * * *", timeout=3600, enable=true
  */
+
 const $ = new Env('京东全民开红包');
 const notify = $.isNode() ? require('./sendNotify') : '';
-
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-
 let cookiesArr = [], cookie = '';
 $.redPacketId = [];
+
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
     cookiesArr.push(jdCookieNode[item])
@@ -35,14 +22,16 @@ if ($.isNode()) {
 } else {
   cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
+
 const JD_API_HOST = 'https://api.m.jd.com/api';
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
   }
-  let res = await getAuthorShareCode()
-  $.authorMyShareIds = [...(res || [])];
+  // let res = await getAuthorShareCode()
+  // $.authorMyShareIds = [...(res || [])];
+
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
@@ -65,6 +54,8 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
       await showMsg();
     }
   }
+
+  // 执行任务
   for (let i = 0; i < cookiesArr.length; i++) {
     cookie = cookiesArr[i];
     $.index = i + 1;
@@ -85,20 +76,23 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
         }
       }
     }
-    if ($.canHelp && ($.authorMyShareIds && $.authorMyShareIds.length)) {
-      console.log(`\n\n有剩余助力机会则给作者进行助力`);
-      for (let j = 0; j < $.authorMyShareIds.length && $.canHelp; j++) {
-        console.log(`\n账号 ${$.index} ${$.UserName} 开始给作者 ${$.authorMyShareIds[j]} 进行助力`)
-        $.max = false;
-        await jinli_h5assist($.authorMyShareIds[j]);
-        await $.wait(2000)
-        if ($.max) {
-          $.authorMyShareIds.splice(j, 1)
-          j--
-          continue
-        }
-      }
-    }
+
+    // 有剩余机会助力作者
+    // if ($.canHelp && ($.authorMyShareIds && $.authorMyShareIds.length)) {
+    //   console.log(`\n\n有剩余助力机会则给作者进行助力`);
+    //   for (let j = 0; j < $.authorMyShareIds.length && $.canHelp; j++) {
+    //     console.log(`\n账号 ${$.index} ${$.UserName} 开始给作者 ${$.authorMyShareIds[j]} 进行助力`)
+    //     $.max = false;
+    //     await jinli_h5assist($.authorMyShareIds[j]);
+    //     await $.wait(2000)
+    //     if ($.max) {
+    //       $.authorMyShareIds.splice(j, 1)
+    //       j--
+    //       continue
+    //     }
+    //   }
+    // }
+
   }
 })()
     .catch((e) => {
@@ -110,9 +104,9 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
 
 async function redPacket() {
   try {
-    //await doLuckDrawFun();//券后9.9抽奖
-    //await taskHomePage();//查询任务列表
-    //await doTask();//领取任务，做任务，领取红包奖励
+    await doLuckDrawFun();//券后9.9抽奖
+    await taskHomePage();//查询任务列表
+    await doTask();//领取任务，做任务，领取红包奖励
     await h5activityIndex();//查询红包基础信息
     await red();//红包任务(发起助力红包,领取助力红包等)
     await h5activityIndex();
@@ -120,15 +114,18 @@ async function redPacket() {
     $.logErr(e);
   }
 }
+
 function showMsg() {
   console.log(`\n\n${$.name}获得红包：${$.discount}元\n\n`);
   // $.msg($.name, '', `${$.name}：${$.discount}元`)
 }
+
 async function doLuckDrawFun() {
   for (let i = 0; i < 3; i++) {
     await doLuckDrawEntrance();
   }
 }
+
 function doLuckDrawEntrance() {
   return new Promise(resolve => {
     const options = {
@@ -175,6 +172,7 @@ function doLuckDrawEntrance() {
     })
   })
 }
+
 async function doTask() {
   if ($.taskHomePageData && $.taskHomePageData.code === 0) {
     $.taskInfo = $.taskHomePageData.data.result.taskInfos;
@@ -580,10 +578,11 @@ function getCcTaskList(functionId, body, type = '1') {
     })
   })
 }
-function getAuthorShareCode(url="https://raw.githubusercontent.com/he1pu/JDHelp/main/zcodes.json") {
+
+function getAuthorShareCode() {
     return new Promise(async resolve => {
         const options = {
-            "url": `${url}`,
+            "url": ``,
             "timeout": 10000,
             "headers": {
                 "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
