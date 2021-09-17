7b8b581745d72c0df20d0ea8f406c5473353bf1e
feverrun <209981505@qq.com>
2021-09-17 23:53:00 +0800

    update

--- jd_joy_steal.js
+++ jd_joy_steal.js
@@ -58,6 +58,7 @@ if ($.isNode() && process.env.jdJoyStealCoin) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
   }
+  cookiesArr.unshift('pt_key=pt_test;pt_pin=pt_test');
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
