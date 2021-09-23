4889ff2ec69c7463e1d5017018aa5bc0c91b88a7
feverrun <209981505@qq.com>
2021-09-23 16:41:36 +0800

    old task

--- gua_opencard36.js
+++ gua_opencard36.js
@@ -34,7 +34,7 @@ guaopencard36="true"
 30 8 * 9 * https://raw.githubusercontent.com/smiek2221/scripts/master/gua_opencard36.js, tag=生活有范 大牌狂欢, enabled=true
 */
 const $ = new Env('生活有范 大牌狂欢');
-const Faker=require('./sign_graphics_validate.js') 
+const Faker=require('./utils/sign_graphics_validate.js')
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 
 const notify = $.isNode() ? require('./sendNotify') : '';
