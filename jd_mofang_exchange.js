99e0c06928fe930a70e8c10a6c8d0804096edd22
feverrun <209981505@qq.com>
2021-10-19 21:23:24 +0800

    update

--- jd_mofang_exchange.js
+++ jd_mofang_exchange.js
@@ -70,7 +70,7 @@ function exchange_redpocket(){
 }
 function msgShow() {
     return new Promise(resolve => {
-        $.msg($.name, '', `【京东账号${$.index}】${$.nickname}\n${$.message}`);
+        $.msg($.name, '', `【京东账号${$.index}】${$.UserName}\n${$.message}`);
         resolve()
     })
 }
