d0c2dfb2e6737e9d6b8805ac65d828c08af816e6
feverrun <209981505@qq.com>
2021-10-29 13:16:58 +0800

    update

--- jd_joy_run.js
+++ jd_joy_run.js
@@ -3,7 +3,9 @@ Last Modified time: 2021-6-6 21:22:37
 宠汪汪邀请助力与赛跑助力脚本，感谢github@Zero-S1提供帮助
 活动入口：京东APP我的-更多工具-宠汪汪
 token时效很短，几个小时就失效了,闲麻烦的放弃就行
+
 每天拿到token后，可一次性运行完毕即可。
+
 互助码friendPin是京东用户名，不是昵称（可在京东APP->我的->设置 查看获得）
 token获取途径：
 1、微信搜索'来客有礼'小程序,登陆京东账号，点击底部的'我的'或者'发现'两处地方,即可获取Token，脚本运行提示token失效后，继续按此方法获取即可
@@ -18,25 +20,21 @@ const $ = new Env('宠汪汪赛跑');
 const zooFaker = require('./utils/JDJRValidator_Pure');
 $.get = zooFaker.injectToRequest2($.get.bind($));
 $.post = zooFaker.injectToRequest2($.post.bind($));
-//宠汪汪赛跑所需token，默认读取作者服务器的
+//宠汪汪赛跑所需token，默认读取自作者服务器
 //需自行抓包，宠汪汪小程序获取token，点击`发现`或`我的`，寻找`^https:\/\/draw\.jdfcloud\.com(\/mirror)?\/\/api\/user\/user\/detail\?openId=`获取token
 let jdJoyRunToken = '';
-
 const isRequest = typeof $request != "undefined"
 const JD_BASE_API = `https://draw.jdfcloud.com//pet`;
+
+
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : {};
-//下面给出好友邀请助力的示例填写规则
-let invite_pins = ['18862988021_p'];
-//下面给出好友赛跑助力的示例填写规则
-let run_pins = ['18862988021_p'];
-//friendsArr内置太多会导致IOS端部分软件重启,可PR过来(此处目的:帮别人助力可得30g狗粮)
-let friendsArr = ['18862988021_p']
-
-
-//IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
 let nowTimes = new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000);
+let invite_pins = ['18862988021_p'];
+let run_pins = ['18862988021_p'];
+let friendsArr = ['18862988021_p']
+
 const headers = {
     'Connection' : 'keep-alive',
     'Accept-Encoding' : 'gzip, deflate, br',
@@ -108,11 +106,9 @@ async function main() {
     for (let i = 0; i < cookiesArr.length; i++) {
         if (cookiesArr[i]) {
             $.validate = '';
-            // const zooFaker = require('./utils/JDJRValidator_Pure');
-            // $.validate = await zooFaker.injectToRequest()
             if ($.isNode()) {
                 if (process.env.JOY_RUN_HELP_MYSELF) {
-                    console.log(`\n赛跑会先给账号内部助力,如您当前账户有剩下助力机会则为lx0301作者助力\n`)
+                    console.log(`\n赛跑会先给账号内部助力,如您当前账户有剩下助力机会则为token维护者助力\n`)
                     let my_run_pins = [];
                     Object.values(jdCookieNode).filter(item => item.match(/pt_pin=([^; ]+)(?=;?)/)).map(item => my_run_pins.push(decodeURIComponent(item.match(/pt_pin=([^; ]+)(?=;?)/)[1])))
                     run_pins = [...new Set(my_run_pins), [...getRandomArrayElements([...run_pins[0].split(',')], [...run_pins[0].split(',')].length)]];
@@ -159,52 +155,6 @@ async function main() {
     $.done()
 }
 //获取来客有礼Token
-let count = 0;
-async function getToken() {
-    const url = $request.url;
-    $.log(`${$.name}url\n${url}\n`)
-    if (isURL(url, /^https:\/\/draw\.jdfcloud\.com(\/mirror)?\/\/api\/user\/addUser\?code=/)) {
-        const body = JSON.parse($response.body);
-        const LKYLToken = body.data && body.data.token;
-        if (LKYLToken) {
-            $.log(`${$.name} token\n${LKYLToken}\n`);
-            $.msg($.name, '更新Token: 成功🎉', ``);
-            console.log(`\nToken，${LKYLToken}\n`)
-            $.http.post({
-                url: `http://hz.feverrun.top:99/share/submit/token`,
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({
-                    "name": "joy",
-                    "lkyltoken": LKYLToken,
-                }),
-                timeout: 30000
-            }).then((resp) => {
-                if (resp.statusCode === 200) {
-                    try {
-                        let { body } = resp;
-                        console.log(`Token提交结果:${body}\n`)
-                        body = JSON.parse(body);
-                        console.log(`${body.message}`)
-                    } catch (e) {
-                        console.log(`提交Token异常:${e}`)
-                    }
-                }
-            }).catch((e) => console.log(`catch 宠汪汪TOKEN提交异常:${e}`));
-            $.setdata(LKYLToken, 'jdJoyRunToken');
-        }
-        $.done({ body: JSON.stringify(body) })
-    } else if (isURL(url, /^https:\/\/draw\.jdfcloud\.com(\/mirror)?\/\/api\/user\/user\/detail\?openId=/)){
-        if ($request && $request.method !== 'OPTIONS') {
-            const LKYLToken = $request.headers['LKYLToken'];
-            $.setdata(LKYLToken, 'jdJoyRunToken');
-            $.msg($.name, '获取Token: 成功🎉', ``);
-            // $.done({ body: JSON.stringify(body) })
-            $.done({ url: url })
-        }
-    } else {
-        $.done()
-    }
-}
 function readToken() {
     return new Promise(resolve => {
         $.get({
@@ -221,8 +171,6 @@ function readToken() {
                     console.log(`${$.name} API请求失败，请检查网路重试`)
                 } else {
                     if (data) {
-                        // if ($.isNode() && !run_pins[0].includes("被折叠的记忆33")) resolve(null);
-                        // console.log(`\n\n搬运我脚本修改我内置互助码的，请不要盗取我服务器token\n\n\n`)
                         data = JSON.parse(data);
                     }
                 }
@@ -465,10 +413,6 @@ function combatDetail(invitePin) {
         });
     })
 }
-function isURL(domain, reg) {
-    // const name = reg;
-    return reg.test(domain);
-}
 function jsonParse(str) {
     if (typeof str == "string") {
         try {
@@ -516,7 +460,7 @@ function getRandomArrayElements(arr, count) {
 //         })
 //     })
 // }
-isRequest ? getToken() : main();
+main();
 
 // md5
 !function(n){function t(n,t){var r=(65535&n)+(65535&t);return(n>>16)+(t>>16)+(r>>16)<<16|65535&r}function r(n,t){return n<<t|n>>>32-t}function e(n,e,o,u,c,f){return t(r(t(t(e,n),t(u,f)),c),o)}function o(n,t,r,o,u,c,f){return e(t&r|~t&o,n,t,u,c,f)}function u(n,t,r,o,u,c,f){return e(t&o|r&~o,n,t,u,c,f)}function c(n,t,r,o,u,c,f){return e(t^r^o,n,t,u,c,f)}function f(n,t,r,o,u,c,f){return e(r^(t|~o),n,t,u,c,f)}function i(n,r){n[r>>5]|=128<<r%32,n[14+(r+64>>>9<<4)]=r;var e,i,a,d,h,l=1732584193,g=-271733879,v=-1732584194,m=271733878;for(e=0;e<n.length;e+=16){i=l,a=g,d=v,h=m,g=f(g=f(g=f(g=f(g=c(g=c(g=c(g=c(g=u(g=u(g=u(g=u(g=o(g=o(g=o(g=o(g,v=o(v,m=o(m,l=o(l,g,v,m,n[e],7,-680876936),g,v,n[e+1],12,-389564586),l,g,n[e+2],17,606105819),m,l,n[e+3],22,-1044525330),v=o(v,m=o(m,l=o(l,g,v,m,n[e+4],7,-176418897),g,v,n[e+5],12,1200080426),l,g,n[e+6],17,-1473231341),m,l,n[e+7],22,-45705983),v=o(v,m=o(m,l=o(l,g,v,m,n[e+8],7,1770035416),g,v,n[e+9],12,-1958414417),l,g,n[e+10],17,-42063),m,l,n[e+11],22,-1990404162),v=o(v,m=o(m,l=o(l,g,v,m,n[e+12],7,1804603682),g,v,n[e+13],12,-40341101),l,g,n[e+14],17,-1502002290),m,l,n[e+15],22,1236535329),v=u(v,m=u(m,l=u(l,g,v,m,n[e+1],5,-165796510),g,v,n[e+6],9,-1069501632),l,g,n[e+11],14,643717713),m,l,n[e],20,-373897302),v=u(v,m=u(m,l=u(l,g,v,m,n[e+5],5,-701558691),g,v,n[e+10],9,38016083),l,g,n[e+15],14,-660478335),m,l,n[e+4],20,-405537848),v=u(v,m=u(m,l=u(l,g,v,m,n[e+9],5,568446438),g,v,n[e+14],9,-1019803690),l,g,n[e+3],14,-187363961),m,l,n[e+8],20,1163531501),v=u(v,m=u(m,l=u(l,g,v,m,n[e+13],5,-1444681467),g,v,n[e+2],9,-51403784),l,g,n[e+7],14,1735328473),m,l,n[e+12],20,-1926607734),v=c(v,m=c(m,l=c(l,g,v,m,n[e+5],4,-378558),g,v,n[e+8],11,-2022574463),l,g,n[e+11],16,1839030562),m,l,n[e+14],23,-35309556),v=c(v,m=c(m,l=c(l,g,v,m,n[e+1],4,-1530992060),g,v,n[e+4],11,1272893353),l,g,n[e+7],16,-155497632),m,l,n[e+10],23,-1094730640),v=c(v,m=c(m,l=c(l,g,v,m,n[e+13],4,681279174),g,v,n[e],11,-358537222),l,g,n[e+3],16,-722521979),m,l,n[e+6],23,76029189),v=c(v,m=c(m,l=c(l,g,v,m,n[e+9],4,-640364487),g,v,n[e+12],11,-421815835),l,g,n[e+15],16,530742520),m,l,n[e+2],23,-995338651),v=f(v,m=f(m,l=f(l,g,v,m,n[e],6,-198630844),g,v,n[e+7],10,1126891415),l,g,n[e+14],15,-1416354905),m,l,n[e+5],21,-57434055),v=f(v,m=f(m,l=f(l,g,v,m,n[e+12],6,1700485571),g,v,n[e+3],10,-1894986606),l,g,n[e+10],15,-1051523),m,l,n[e+1],21,-2054922799),v=f(v,m=f(m,l=f(l,g,v,m,n[e+8],6,1873313359),g,v,n[e+15],10,-30611744),l,g,n[e+6],15,-1560198380),m,l,n[e+13],21,1309151649),v=f(v,m=f(m,l=f(l,g,v,m,n[e+4],6,-145523070),g,v,n[e+11],10,-1120210379),l,g,n[e+2],15,718787259),m,l,n[e+9],21,-343485551),l=t(l,i),g=t(g,a),v=t(v,d),m=t(m,h)}return[l,g,v,m]}function a(n){var t,r="",e=32*n.length;for(t=0;t<e;t+=8){r+=String.fromCharCode(n[t>>5]>>>t%32&255)}return r}function d(n){var t,r=[];for(r[(n.length>>2)-1]=void 0,t=0;t<r.length;t+=1){r[t]=0}var e=8*n.length;for(t=0;t<e;t+=8){r[t>>5]|=(255&n.charCodeAt(t/8))<<t%32}return r}function h(n){return a(i(d(n),8*n.length))}function l(n,t){var r,e,o=d(n),u=[],c=[];for(u[15]=c[15]=void 0,o.length>16&&(o=i(o,8*n.length)),r=0;r<16;r+=1){u[r]=909522486^o[r],c[r]=1549556828^o[r]}return e=i(u.concat(d(t)),512+8*t.length),a(i(c.concat(e),640))}function g(n){var t,r,e="";for(r=0;r<n.length;r+=1){t=n.charCodeAt(r),e+="0123456789abcdef".charAt(t>>>4&15)+"0123456789abcdef".charAt(15&t)}return e}function v(n){return unescape(encodeURIComponent(n))}function m(n){return h(v(n))}function p(n){return g(m(n))}function s(n,t){return l(v(n),v(t))}function C(n,t){return g(s(n,t))}function A(n,t,r){return t?r?s(t,n):C(t,n):r?m(n):p(n)}$.md5=A}(this);
