0dd2d9c6504e459a23f3cbe032dc4d6079fee800
feverrun <209981505@qq.com>
2021-12-13 06:57:36 +0800

    update

--- jd_fanli.js
+++ jd_fanli.js
@@ -2,21 +2,27 @@
 /*
 京东饭粒
 已支持IOS双京东账号,Node.js支持N个京东账号
-cron "20 0,9,17 * * *" script-path=jd_fanli.js,tag=京东饭粒
- */
+[task_local]
+#京东饭粒
+14 0,9,17 * * * jd_fanli.js, tag=京东饭粒, img-url=x.png, enabled=true
+
+================Loon==============
+[Script]
+cron "14 0,9,17 * * *" script-path=jd_fanli.js,tag=京东饭粒
+*/
 
 const $ = new Env('京东饭粒');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
-let cookiesArr = [], cookie = '', message = '', personMessage = '';
+let cookiesArr = [], cookie = '', message = '',personMessage='';
 
-let lz_cookie = {};
+let lz_cookie = {}
+
 if ($.isNode()) {
     Object.keys(jdCookieNode).forEach((item) => {
         cookiesArr.push(jdCookieNode[item])
     })
-    if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false')
-        console.log = () => {};
+    if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => { };
 } else {
     let cookiesData = $.getdata('CookiesJD') || "[]";
     cookiesData = JSON.parse(cookiesData);
@@ -26,11 +32,9 @@ if ($.isNode()) {
     cookiesArr.reverse();
     cookiesArr = cookiesArr.filter(item => !!item);
 }
-!(async() => {
+!(async () => {
     if (!cookiesArr[0]) {
-        $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {
-            "open-url": "https://bean.m.jd.com/bean/signIndex.action"
-        });
+        $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
         return;
     }
     for (let i = 0; i < cookiesArr.length; i++) {
@@ -63,8 +67,10 @@ if ($.isNode()) {
                 let range = $.count.maxTaskCount - $.count.finishCount;
                 await getTaskList(cookie)
                 await $.wait(2000)
+                var CountDoTask =0;
                 for (let k in $.taskList) {
-                    if ($.taskList[k].taskId !== null && $.taskList[k].status == 1) {
+                    if ($.taskList[k].taskId !== null && $.taskList[k].statusName != "活动结束" && $.taskList[k].statusName != "明日再来") {
+                        CountDoTask+=0;
                         console.log(`开始尝试活动:` + $.taskList[k].taskName);
                         await saveTaskRecord(cookie, $.taskList[k].taskId, $.taskList[k].businessId, $.taskList[k].taskType);
                         if ($.sendBody) {
@@ -73,15 +79,13 @@ if ($.isNode()) {
                         } else {
                             continue;
                         }
-                        if ($.count.finishCount = $.count.maxTaskCount) {
+                        if ($.count.finishCount == $.count.maxTaskCount) {
                             console.log(`任务全部完成!`);
                             break;
                         }
                     }
 
                 }
-
-
             } else {
                 console.log("任务已做完")
             }
@@ -97,7 +101,7 @@ if ($.isNode()) {
         $.done();
     })
 
-function saveTaskRecord(ck, taskId, businessId, taskType) {
+function saveTaskRecord(ck,taskId,businessId,taskType) {
     let opt = {
         url: `https://ifanli.m.jd.com/rebateapi/task/saveTaskRecord`,
         headers: {
@@ -115,11 +119,7 @@ function saveTaskRecord(ck, taskId, businessId, taskType) {
             "Cookie": ck,
             "Content-Type": "application/json;charset=UTF-8"
         },
-        body: JSON.stringify({
-            taskId: taskId,
-            businessId: businessId,
-            taskType: taskType
-        }),
+        body : JSON.stringify({ taskId: taskId,businessId:businessId, taskType: taskType }),
 
     }
     return new Promise(resolve => {
@@ -132,11 +132,13 @@ function saveTaskRecord(ck, taskId, businessId, taskType) {
                     if (data) {
                         data = JSON.parse(data);
                         // console.log(data,"获取id")
-                        if (data.content) {
+                        if(data.content){
                             $.sendBody = data.content
-                        } else {
+                        }
+                        else{
                             console.log("未获取到活动内容，开始下一个")
                         }
+
 
                     } else {
                         $.log("京东返回了空数据")
@@ -145,15 +147,14 @@ function saveTaskRecord(ck, taskId, businessId, taskType) {
             } catch (error) {
                 $.log(error)
                 // console.log(error)
-            }
-            finally {
+            } finally {
                 resolve();
             }
         })
     })
 }
 
-function saveTaskRecord1(ck, taskId, businessId, taskType, uid, tt) {
+function saveTaskRecord1(ck,taskId,businessId,taskType,uid,tt) {
     let opt = {
         url: `https://ifanli.m.jd.com/rebateapi/task/saveTaskRecord`,
         headers: {
@@ -171,13 +172,7 @@ function saveTaskRecord1(ck, taskId, businessId, taskType, uid, tt) {
             "Cookie": ck,
             "Content-Type": "application/json;charset=UTF-8"
         },
-        body: JSON.stringify({
-            taskId: taskId,
-            taskType: taskType,
-            businessId: businessId,
-            uid: uid,
-            tt: tt
-        }),
+        body : JSON.stringify({ taskId: taskId, taskType: taskType,businessId:businessId,uid:uid,tt:tt }),
 
     }
     return new Promise(resolve => {
@@ -201,8 +196,7 @@ function saveTaskRecord1(ck, taskId, businessId, taskType, uid, tt) {
             } catch (error) {
                 $.log(error)
                 // console.log(error)
-            }
-            finally {
+            } finally {
                 resolve();
             }
         })
@@ -212,7 +206,7 @@ function saveTaskRecord1(ck, taskId, businessId, taskType, uid, tt) {
 function getTaskFinishCount(ck) {
     return new Promise(resolve => {
         const options = {
-            url: 'https://ifanli.m.jd.com/rebateapi/task/getTaskFinishCount',
+            url:'https://ifanli.m.jd.com/rebateapi/task/getTaskFinishCount',
             headers: {
                 "Host": "ifanli.m.jd.com",
                 "Connection": "keep-alive",
@@ -229,22 +223,21 @@ function getTaskFinishCount(ck) {
                 "Content-Type": "application/json;charset=UTF-8"
             },
         };
-        $.get(options, async(err, resp, data) => {
+        $.get(options, async (err, resp, data) => {
             try {
                 if (err) {
                     $.log(err)
                 } else {
-                    if (data) {
+                    if (data){
                         data = JSON.parse(data)
                         // console.log(data)
-                        console.log("已完成次数：" + data.content.finishCount + "  总任务次数：" + data.content.maxTaskCount)
-                        $.count = data.content
+                        console.log("已完成次数："+data.content.finishCount+"  总任务次数："+data.content.maxTaskCount)
+                        $.count=data.content
                     }
                 }
             } catch (e) {
                 $.logErr(e, resp)
-            }
-            finally {
+            } finally {
                 resolve(data);
             }
         })
@@ -254,7 +247,7 @@ function getTaskFinishCount(ck) {
 function getTaskList(ck) {
     return new Promise(resolve => {
         const options = {
-            url: 'https://ifanli.m.jd.com/rebateapi/task/getTaskList',
+            url:'https://ifanli.m.jd.com/rebateapi/task/getTaskList',
             headers: {
                 "Host": "ifanli.m.jd.com",
                 "Connection": "keep-alive",
@@ -271,17 +264,18 @@ function getTaskList(ck) {
                 "Content-Type": "application/json;charset=UTF-8"
             },
         };
-        $.get(options, async(err, resp, data) => {
+        $.get(options, async (err, resp, data) => {
             try {
                 if (err) {
                     $.log(err)
                 } else {
-                    if (data) {
+                    if (data){
                         data = JSON.parse(data)
                         // console.log(data,"活动列表")
-                        if (data.content) {
-                            $.taskList = data.content
-                        } else {
+                        if(data.content){
+                            $.taskList=data.content
+                        }
+                        else{
                             console.log("未获取到活动列表，请检查活动")
                         }
 
@@ -289,8 +283,7 @@ function getTaskList(ck) {
                 }
             } catch (e) {
                 $.logErr(e, resp)
-            }
-            finally {
+            } finally {
                 resolve(data);
             }
         })
@@ -331,14 +324,12 @@ function checkCookie() {
                 }
             } catch (e) {
                 $.logErr(e)
-            }
-            finally {
+            } finally {
                 resolve();
             }
         })
     })
 }
-
 
 // prettier-ignore
 !function (n) { "use strict"; function t(n, t) { var r = (65535 & n) + (65535 & t); return (n >> 16) + (t >> 16) + (r >> 16) << 16 | 65535 & r } function r(n, t) { return n << t | n >>> 32 - t } function e(n, e, o, u, c, f) { return t(r(t(t(e, n), t(u, f)), c), o) } function o(n, t, r, o, u, c, f) { return e(t & r | ~t & o, n, t, u, c, f) } function u(n, t, r, o, u, c, f) { return e(t & o | r & ~o, n, t, u, c, f) } function c(n, t, r, o, u, c, f) { return e(t ^ r ^ o, n, t, u, c, f) } function f(n, t, r, o, u, c, f) { return e(r ^ (t | ~o), n, t, u, c, f) } function i(n, r) { n[r >> 5] |= 128 << r % 32, n[14 + (r + 64 >>> 9 << 4)] = r; var e, i, a, d, h, l = 1732584193, g = -271733879, v = -1732584194, m = 271733878; for (e = 0; e < n.length; e += 16)i = l, a = g, d = v, h = m, g = f(g = f(g = f(g = f(g = c(g = c(g = c(g = c(g = u(g = u(g = u(g = u(g = o(g = o(g = o(g = o(g, v = o(v, m = o(m, l = o(l, g, v, m, n[e], 7, -680876936), g, v, n[e + 1], 12, -389564586), l, g, n[e + 2], 17, 606105819), m, l, n[e + 3], 22, -1044525330), v = o(v, m = o(m, l = o(l, g, v, m, n[e + 4], 7, -176418897), g, v, n[e + 5], 12, 1200080426), l, g, n[e + 6], 17, -1473231341), m, l, n[e + 7], 22, -45705983), v = o(v, m = o(m, l = o(l, g, v, m, n[e + 8], 7, 1770035416), g, v, n[e + 9], 12, -1958414417), l, g, n[e + 10], 17, -42063), m, l, n[e + 11], 22, -1990404162), v = o(v, m = o(m, l = o(l, g, v, m, n[e + 12], 7, 1804603682), g, v, n[e + 13], 12, -40341101), l, g, n[e + 14], 17, -1502002290), m, l, n[e + 15], 22, 1236535329), v = u(v, m = u(m, l = u(l, g, v, m, n[e + 1], 5, -165796510), g, v, n[e + 6], 9, -1069501632), l, g, n[e + 11], 14, 643717713), m, l, n[e], 20, -373897302), v = u(v, m = u(m, l = u(l, g, v, m, n[e + 5], 5, -701558691), g, v, n[e + 10], 9, 38016083), l, g, n[e + 15], 14, -660478335), m, l, n[e + 4], 20, -405537848), v = u(v, m = u(m, l = u(l, g, v, m, n[e + 9], 5, 568446438), g, v, n[e + 14], 9, -1019803690), l, g, n[e + 3], 14, -187363961), m, l, n[e + 8], 20, 1163531501), v = u(v, m = u(m, l = u(l, g, v, m, n[e + 13], 5, -1444681467), g, v, n[e + 2], 9, -51403784), l, g, n[e + 7], 14, 1735328473), m, l, n[e + 12], 20, -1926607734), v = c(v, m = c(m, l = c(l, g, v, m, n[e + 5], 4, -378558), g, v, n[e + 8], 11, -2022574463), l, g, n[e + 11], 16, 1839030562), m, l, n[e + 14], 23, -35309556), v = c(v, m = c(m, l = c(l, g, v, m, n[e + 1], 4, -1530992060), g, v, n[e + 4], 11, 1272893353), l, g, n[e + 7], 16, -155497632), m, l, n[e + 10], 23, -1094730640), v = c(v, m = c(m, l = c(l, g, v, m, n[e + 13], 4, 681279174), g, v, n[e], 11, -358537222), l, g, n[e + 3], 16, -722521979), m, l, n[e + 6], 23, 76029189), v = c(v, m = c(m, l = c(l, g, v, m, n[e + 9], 4, -640364487), g, v, n[e + 12], 11, -421815835), l, g, n[e + 15], 16, 530742520), m, l, n[e + 2], 23, -995338651), v = f(v, m = f(m, l = f(l, g, v, m, n[e], 6, -198630844), g, v, n[e + 7], 10, 1126891415), l, g, n[e + 14], 15, -1416354905), m, l, n[e + 5], 21, -57434055), v = f(v, m = f(m, l = f(l, g, v, m, n[e + 12], 6, 1700485571), g, v, n[e + 3], 10, -1894986606), l, g, n[e + 10], 15, -1051523), m, l, n[e + 1], 21, -2054922799), v = f(v, m = f(m, l = f(l, g, v, m, n[e + 8], 6, 1873313359), g, v, n[e + 15], 10, -30611744), l, g, n[e + 6], 15, -1560198380), m, l, n[e + 13], 21, 1309151649), v = f(v, m = f(m, l = f(l, g, v, m, n[e + 4], 6, -145523070), g, v, n[e + 11], 10, -1120210379), l, g, n[e + 2], 15, 718787259), m, l, n[e + 9], 21, -343485551), l = t(l, i), g = t(g, a), v = t(v, d), m = t(m, h); return [l, g, v, m] } function a(n) { var t, r = "", e = 32 * n.length; for (t = 0; t < e; t += 8)r += String.fromCharCode(n[t >> 5] >>> t % 32 & 255); return r } function d(n) { var t, r = []; for (r[(n.length >> 2) - 1] = void 0, t = 0; t < r.length; t += 1)r[t] = 0; var e = 8 * n.length; for (t = 0; t < e; t += 8)r[t >> 5] |= (255 & n.charCodeAt(t / 8)) << t % 32; return r } function h(n) { return a(i(d(n), 8 * n.length)) } function l(n, t) { var r, e, o = d(n), u = [], c = []; for (u[15] = c[15] = void 0, o.length > 16 && (o = i(o, 8 * n.length)), r = 0; r < 16; r += 1)u[r] = 909522486 ^ o[r], c[r] = 1549556828 ^ o[r]; return e = i(u.concat(d(t)), 512 + 8 * t.length), a(i(c.concat(e), 640)) } function g(n) { var t, r, e = ""; for (r = 0; r < n.length; r += 1)t = n.charCodeAt(r), e += "0123456789abcdef".charAt(t >>> 4 & 15) + "0123456789abcdef".charAt(15 & t); return e } function v(n) { return unescape(encodeURIComponent(n)) } function m(n) { return h(v(n)) } function p(n) { return g(m(n)) } function s(n, t) { return l(v(n), v(t)) } function C(n, t) { return g(s(n, t)) } function A(n, t, r) { return t ? r ? s(t, n) : C(t, n) : r ? m(n) : p(n) } $.md5 = A }(this);
