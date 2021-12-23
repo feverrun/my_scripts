7ae60725108738c7bb8d6b239550baf621c78cd4
feverrun <209981505@qq.com>
2021-12-23 18:20:31 +0800

    update

--- backUp/activity/jd_joy.js
+++ backUp/activity/jd_joy.js
@@ -1,12 +1,10 @@
 /*
-脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
-IOS用户支持京东双账号,NodeJs用户支持N个京东账号
 更新时间：2021-6-6
 活动入口：京东APP我的-更多工具-宠汪汪
 建议先凌晨0点运行jd_joy.js脚本获取狗粮后，再运行此脚本(jd_joy_steal.js)可偷好友积分，6点运行可偷好友狗粮
 feedCount:自定义 每次喂养数量; 等级只和喂养次数有关，与数量无关
 推荐每次投喂10个，积累狗粮，然后去玩聚宝盆赌
-cron "15 0-23/2 * * *" script-path=jd_joy.js,tag=京东宠汪汪
+cron "35 5,7,8,12,18,20 * * *" script-path=jd_joy.js,tag=京东宠汪汪
 */
 const $ = new Env('宠汪汪');
 const zooFaker = require('./utils/JDJRValidator_Pure');
