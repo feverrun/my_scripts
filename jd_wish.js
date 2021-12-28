8da0c10f758e204ff923e4269f9a1871631cb546
feverrun <209981505@qq.com>
2021-12-28 12:36:08 +0800

    update

--- jd_wish.js
+++ jd_wish.js
@@ -10,8 +10,8 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let message = '', allMessage = '';
 let cookiesArr = [], cookie = '';
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-let appIdArr = ['1E1NXxq0', '1FFVQyqw','1F11Vx64'];
-let appNameArr = ['众筹许愿池', '1111点心动','冰雪闪耀季'];
+let appIdArr = ['1E1NXxq0', '1FFVQyqw',];
+let appNameArr = ['众筹许愿池', '1111点心动',];
 
 let appId, appName;
 $.shareCode = [];
