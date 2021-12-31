7593b82a43f09adea56886d60b504fb74353101a
feverrun <209981505@qq.com>
2021-12-31 12:36:48 +0800

    update

--- jd_opencard86.js
+++ jd_opencard86.js
@@ -29,7 +29,7 @@ All变量适用
 
 [task_local]
 #12.20~1.5 双旦寻宝 玩转大富翁
-18 1,10 20-31,2-5 12,1 * jd_opencard86.js, tag=12.20~1.5 双旦寻宝 玩转大富翁, enabled=true
+18 1,10 20-31,1-5 12,1 * jd_opencard86.js, tag=12.20~1.5 双旦寻宝 玩转大富翁, enabled=true
 
 */
 let opencard_addSku = "true"
