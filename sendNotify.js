aced47eeab17b0c5932300697c5141ed9b9f19f1
feverrun <209981505@qq.com>
2022-01-16 11:00:35 +0800

    update

--- sendNotify.js
+++ sendNotify.js
@@ -8,7 +8,7 @@ const querystring = require('querystring');
 const exec = require('child_process').exec;
 const $ = new Env();
 const timeout = 15000; //超时时间(单位毫秒)
-console.log("加载sendNotify，当前版本: 20220112");
+console.log("加载sendNotify，当前版本: 20220116");
 // =======================================go-cqhttp通知设置区域===========================================
 //gobot_url 填写请求地址http://127.0.0.1/send_private_msg
 //gobot_token 填写在go-cqhttp文件设置的访问密钥
@@ -257,7 +257,14 @@ async function sendNotify(text, desp, params = {}, author = '\n', strsummary = "
                             isLogin = true;
                             await checkCookie(temptest.value);
                             if (!isLogin) {
-                                const DisableCkBody = await DisableCk(temptest._id);
+                                var tempid = 0;
+                                if (temptest._id) {
+                                    tempid = temptest._id;
+                                }
+                                if (temptest.id) {
+                                    tempid =temptest.id;
+                                }
+                                const DisableCkBody = await DisableCk(tempid);
                                 strPtPin = temptest.value;
                                 strPtPin = (strPtPin.match(/pt_pin=([^; ]+)(?=;?)/) && strPtPin.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
                                 var strAllNotify = "";
