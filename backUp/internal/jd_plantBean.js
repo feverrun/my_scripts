9fc93d36b204093a02b55d8795e3bdf6258e623f
feverrun <209981505@qq.com>
2022-03-18 09:56:38 +0800

    fix

--- backUp/internal/jd_plantBean.js
+++ backUp/internal/jd_plantBean.js
@@ -271,16 +271,22 @@ async function doTask() {
                 const { data } = $.shopTaskListRes;
                 let goodShopListARR = [], moreShopListARR = [], shopList = [];
                 const { goodShopList, moreShopList } = data;
-                for (let i of goodShopList) {
-                    if (i.taskState === '2') {
-                        goodShopListARR.push(i);
+
+                if (goodShopList) {
+                    for (let i of goodShopList) {
+                        if (i.taskState === '2') {
+                            goodShopListARR.push(i);
+                        }
                     }
                 }
-                for (let j of moreShopList) {
-                    if (j.taskState === '2') {
-                        moreShopListARR.push(j);
+                if (moreShopList) {
+                    for (let j of moreShopList) {
+                        if (j.taskState === '2') {
+                            moreShopListARR.push(j);
+                        }
                     }
                 }
+
                 shopList = goodShopListARR.concat(moreShopListARR);
                 for (let shop of shopList) {
                     const { shopId, shopTaskId } = shop;
