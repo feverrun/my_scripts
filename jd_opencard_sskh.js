a134d62c0cebc4f1fb563d9c7f610b3fe3a22cd5
feverrun <209981505@qq.com>
2022-06-10 12:59:50 +0800

    card

--- jd_opencard_sskh.js
+++ jd_opencard_sskh.js
@@ -1,7 +1,7 @@
 /*
 5.31~6.20 时尚狂欢盛典
 https://lzdz1-isv.isvjcloud.com/dingzhi/may/fashionUnion/activity/657324?activityId=dz61218d9945da9fd2f4164d2aad92
-cron "5 13 * * *" jd_opencard_ssk.js
+cron "1 1 1 1 1" jd_opencard_sskh.js
 */
 
 const $ = new Env('5.31~6.20 时尚狂欢盛典')
