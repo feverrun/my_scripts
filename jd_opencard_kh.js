5a205406840929185a4ec5be2817bc757af47950
feverrun <209981505@qq.com>
2022-06-14 18:35:51 +0800

    wdz

--- jd_opencard_kh.js
+++ jd_opencard_kh.js
@@ -1,9 +1,9 @@
 /*
-时尚狂欢盛典
+6.13-6.20 时尚狂欢盛典
 https://lzdz1-isv.isvjcloud.com/dingzhi/june/fashionUnion/activity/657324?activityId=dz19181d9f4878be1fedb3c54e714b
 cron "1 1 1 1 1" jd_opencard_kh.js
 */
-const $ = new Env('时尚狂欢盛典')
+const $ = new Env('6.13-6.20 时尚狂欢盛典')
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
 
