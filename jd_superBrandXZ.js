818d6b740be162b53b26fbc1f1c50752b972731c
feverrun <209981505@qq.com>
2022-07-30 00:46:51 +0800

    card

--- jd_superBrandXZ.js
+++ jd_superBrandXZ.js
@@ -34,7 +34,7 @@ if ($.isNode()) {
         $.isLogin = true;
         $.nickName = '';
         $.UserName = decodeURIComponent($.cookie.match(/pt_pin=([^; ]+)(?=;?)/) && $.cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
-        await TotalBean();
+        //await TotalBean();
         console.log(`\n*****开始【京东账号${$.index}】${$.nickName || $.UserName}*****\n`);
         if (!$.isLogin) {
             $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
@@ -59,7 +59,7 @@ async function main() {
     $.runFlag = false;
     $.activityInfo = {};
     await takeRequest('showBadgeInfo');
-    if($.bizCode == 'MP001'){
+    if ($.bizCode == 'MP001') {
         console.log(`本期活动结束，等待下期。。。`);
         $.flag = true
         return;
@@ -74,6 +74,20 @@ async function main() {
     await takeRequest('superBrandTaskList', { "source": "badge", "activityId": $.activityId });
     await $.wait(1000);
     await doTask();
+    if (new Date().getHours() >= 20) {
+        console.log(`去瓜分`);
+        if ($.activityInfo.activityBadgeInfo.allTaskStatus === 1) {
+            if ($.activityInfo.activityBadgeInfo.divideStatus === 0) {
+                await takeRequest('superBrandTaskLottery', { "source": "badge", "activityId": $.activityId, "encryptProjectId": $.encryptProjectId, "tag": "divide" });
+            } else {
+                $.log('已瓜分过啦！')
+            }
+        } else {
+            $.log('未获得瓜分资格');
+        }
+    } else {
+        console.log('未到瓜分时间！')
+    }
 }
 
 
@@ -166,7 +180,7 @@ function dealReturn(type, data) {
     switch (type) {
         case 'showBadgeInfo':
             $.bizCode = data.data.bizCode;
-            if (data.code === '0' && data.data && data.data.result) {
+            if (data.code === '0' &&  data.data?.result) {
                 $.activityInfo = data.data.result;
             }
             break;
@@ -177,13 +191,22 @@ function dealReturn(type, data) {
             break;
         case 'superBrandDoTask':
             if (data.code === '0') {
-                console.log(JSON.stringify(data.data.bizMsg));
+                console.log(data.data.bizMsg);
             } else {
-                console.log(JSON.stringify(data));
+                console.log(data);
+            }
+            break;
+        case 'superBrandTaskLottery':
+            if (data.data.success) {
+                if (data.data?.result?.rewardComponent?.successRewards) {
+                    console.log(`获得豆子：${data.data.result.rewardComponent.beanList[0].quantity}`)
+                }
+            } else {
+                console.log(data.bizMsg);
             }
             break;
         default:
-            console.log(JSON.stringify(data));
+            console.log(data);
     }
 }
 
