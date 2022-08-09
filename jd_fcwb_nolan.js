33c07bacaba691a1fd4b95997c96d92746d2af66
feverrun <209981505@qq.com>
2022-08-09 23:40:17 +0800

    nolan

--- jd_fcwb_nolan.js
+++ jd_fcwb_nolan.js
@@ -14,7 +14,7 @@ export FCWB_INVITER_CODE=""
 跑之前一定要去手动挖宝，先开两个格子, 开到优惠券就是活动黑了
 cron "1 1 1 1 1" jd_fcwb_nolan.js
  */
-const $ = new Env('发财挖宝');
+const $ = new Env('发财挖宝-nolan版');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
 
