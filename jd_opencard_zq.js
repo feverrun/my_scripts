6bf563f4ca2c98cf7673c31747f5f7d98e6a3a9c
feverrun <209981505@qq.com>
2022-09-06 00:56:52 +0800

    rename

--- jd_opencard_zq.js
+++ jd_opencard_zq.js
@@ -1,7 +1,7 @@
 /*
 月满中秋  有礼相伴
 export OPENCARD_DRAW_NUM="5"  是否抽奖 抽奖次数 不推荐填写。
-cron "6 1 * * *" jd_opencard_ym.js
+cron "1 1 1 1 1" jd_opencard_zq.js
 */
 const $ = new Env('月满中秋 有礼相伴')
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
