e7659ae6954e4fe5020a1f2ba8ded97fbb2887a4
feverrun <209981505@qq.com>
2022-11-05 00:14:05 +0800

    card

--- jd_opencard_pl4.js
+++ jd_opencard_pl4.js
@@ -1,7 +1,7 @@
 /*
 品类联合4
 export jd_opencard_categoryUnion_draw="1" 不自动抽奖
-cron "1 1 1 1 1" jd_opencard_categoryUnion.js
+cron "1 1 1 1 1" jd_opencard_pl4.js
 */
 const $ = new Env('品类联合4');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
