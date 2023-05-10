2979f91cff9e885c729e2cd11402e6c56021191c
feverrun <12291033+feverrun@users.noreply.github.com>
2023-05-11 00:48:39 +0800

    fix

--- jdCookie.js
+++ jdCookie.js
@@ -4,28 +4,31 @@
  */
 let CookieJDs = []
 
-// 判断环境变量里面是否有京东ck
+// modified by proenv
 if (process.env.JD_COOKIE) {
-    if (process.env.JD_COOKIE.indexOf('&') > -1) {
-        CookieJDs = process.env.JD_COOKIE.split('&');
-    } else if (process.env.JD_COOKIE.indexOf('\n') > -1) {
-        CookieJDs = process.env.JD_COOKIE.split('\n');
+    let jd_cookies = process.env.JD_COOKIE;
+    jd_cookies = jd_cookies.replace(/\s+/g, "");
+    if (jd_cookies.indexOf('&') > -1) {
+        CookieJDs = jd_cookies.split('&');
+    } else if (jd_cookies.indexOf('\n') > -1) {
+        CookieJDs = jd_cookies.split('\n');
     } else {
-        CookieJDs = [process.env.JD_COOKIE];
+        CookieJDs = [jd_cookies];
     }
 }
 
-if (JSON.stringify(process.env).indexOf('GITHUB') > -1) {
-    console.log(`请勿使用github action运行此脚本,无论你是从你自己的私库还是其他哪里拉取的源代码，都会导致我被封号\n`);
-    !(async () => {
-        await require('./sendNotify').sendNotify('提醒', `请勿使用github action、滥用github资源会封我仓库以及账号`)
-        await process.exit(0);
-    })()
-}
+// if (JSON.stringify(process.env).indexOf('GITHUB') > -1) {
+//     console.log(`请勿使用github action运行此脚本,无论你是从你自己的私库还是其他哪里拉取的源代码，都会导致我被封号\n`);
+//     !(async () => {
+//         await require('./sendNotify').sendNotify('提醒', `请勿使用github action、滥用github资源会封我仓库以及账号`)
+//         await process.exit(0);
+//     })()
+// }
 
 CookieJDs = [...new Set(CookieJDs.filter(item => !!item))]
 console.log(`\n===============共${CookieJDs.length}个京东账号Cookie===============\n`);
-if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {};
+if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {
+};
 
 for (let i = 0; i < CookieJDs.length; i++) {
     if (!CookieJDs[i].match(/pt_pin=(.+?);/) || !CookieJDs[i].match(/pt_key=(.+?);/)) console.log(`\n提示:京东cookie 【${CookieJDs[i]}】填写不规范,可能会影响部分脚本正常使用。正确格式为: pt_key=xxx;pt_pin=xxx;（分号;不可少）\n`);
