00851cda9a54b78ce1db49b22fe47cb0521dee38
freesoul <222222@qq.com>
2024-05-01 22:15:18 +0800

    update

--- jd_opencard_dp0501.js
+++ jd_opencard_dp0501.js
@@ -6,7 +6,7 @@ export DPLHTY_VIEWSHOP="1"          #是否做浏览任务, 1做，0不做， �
 export DPLHTY_CODE=""               #全部助力给这个邀请码
 cron "7 7 1 5 *" jd_opencard_dp0501.js
 */
-const $ = new Env('大牌0423 5.1-5.10');
+const $ = new Env('大牌0501 5.1-5.10');
 
 
 
