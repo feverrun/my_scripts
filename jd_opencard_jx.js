9653de74e15797b0710a8977575965041b3f7881
freesoul <222222@qq.com>
2024-07-15 09:15:50 +0800

    update

--- jd_opencard_jx.js
+++ jd_opencard_jx.js
@@ -1,9 +1,9 @@
 /*
 export jd_lzdz1_openCard="0"                #卡是否开卡 0-不开卡  1-开卡  默认执行开卡
 export jd_card_waitTime="60"                #表示账号之间延迟, 填60表示账号之间延迟60秒
-cron "6 0,10 15-17 7 *" jd_opencard_dj.js
+cron "6 0,10 15-17 7 *" jd_opencard_jx.js
 */
-const $ = new Env('7.15-7.22 大牌低价 酷爽一夏')
+const $ = new Env('7.15-7.22 夏日有礼 大牌惊喜')
 
 
 
