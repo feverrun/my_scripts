6b9d67ffcbff01cc85b297e677ed1de6eafd183c
freesoul <222222@qq.com>
2024-10-17 19:17:31 +0800

    update

---
+++ utils/Rebels_jdCommon.js
@@ -0,0 +1,5 @@
+//此文件主要用来应对全局sendNotify差异导致依赖缺失问题
+//此文件主要用来应对全局sendNotify差异导致依赖缺失问题
+module.exports.genUA = function (ua=''){
+    return 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.49(0x1800312d) NetType/4G Language/zh_CN'
+}
