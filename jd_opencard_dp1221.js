af23a9f2df5af6ed5bc9d088fcc3c08b34291de2
freesoul <222222@qq.com>
2024-12-21 00:37:39 +0800

    e

--- jd_opencard_dp1221.js
+++ jd_opencard_dp1221.js
@@ -4,7 +4,7 @@ export DPLHTY_ADDCART="1"           #是否做一键加购任务, 1做，0不做
 export DPLHTY_COLLECTSHOP="1"       #是否做一键关注任务, 1做，0不做，默认为1
 export DPLHTY_VIEWSHOP="1"          #是否做浏览任务, 1做，0不做， 默认为1
 export DPLHTY_CODE=""               #全部助力给这个邀请码
-cron "5 1 21 12 *" jd_opencard_dp1221.js
+cron "2 2 2 2 2" jd_opencard_dp1221.js
 */
 const $ = new Env('大牌1221 12.21-12.31');
 
